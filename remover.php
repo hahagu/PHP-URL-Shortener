1f3ef8023944fcc1be87d1f9cc6b8d24b7c1ec47
Min Gu Kang <mingu2001@gmail.com>
2017-08-12 10:11:41 +0900

    Create remover.php

---
+++ remover.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<title>URL shortener</title>
+<meta name="robots" content="noindex, nofollow">
+</html>
+<body>
+<form method="post" action="remove.php" id="remover">
+<label for="longurl">Shortlink Alias to remove</label> <input type="text" name="shorturl" id="shorturl"> <input type="submit" value="Remove">
+</form>
+</form>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+<script>
+$(function () {
+	$('#remover').submit(function () {
+		$.ajax({data: {shorturl: $('#shorturl').val()}, url: 'remove.php', complete: function (XMLHttpRequest, textStatus) {
+			$('#shorturl').val(XMLHttpRequest.responseText);
+		}});
+		return false;
+	});
+});
+</script>
+</body>
+</html>
